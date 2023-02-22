f1be2c553faade6fccd1346351b1c1a69dcf4d3f
Vidhi Makani <vidhimakani007@gmail.com>
2023-02-22 09:52:51 +0530

    initial commit- Done

---
+++ The Light Bulb/app.js
@@ -0,0 +1,11 @@
+ var state = 0;
+		function changeBulbImageon() {
+			
+				document.getElementById("bulbImg").src="https://image.ibb.co/cBBaeR/light_bulb_on.png";
+				
+		}
+        function changeBulbImageoff() {
+			
+            document.getElementById("bulbImg").src="https://image.ibb.co/hoBxtm/light_bulb_off.png";
+    }
+
